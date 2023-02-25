057edaadea19b4e527fdb916274bdf92aa25460d
Josue Hernandez <josuehdz0@gmail.com>
2023-02-25 01:38:40 -0700

    Have getProfilebyId written

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -1,10 +1,22 @@
 <template>
         <div class="border border-dark">
           <div class="row">
+            <div class="col-2">
+              <img :src="post.creator.picture" :alt="post.creator.name" class="profile-picture"
+          :title="`Go to your {{post.creator.name}} page!`">
+            </div>
+            <div class="col-5 d-flex align-items-center">
+              <div class="row">
+                <div> {{post.creator.name}} </div> 
+              </div>
+              <div class="row">
+                <p>graduated</p>
+              </div>
+            
+
+            </div>
             <!-- <img :src="post.creator.picture" :alt="post.creator.name" class="profile-picture"
           :title="`Go to ${post.creator.name}'s profile page!`"> -->
-          <div class="mdi mdi-account"> {{post.creator.name}} </div> 
-          <p>graduated</p>
           </div>
           <div class="row">
             <p>
@@ -37,5 +49,10 @@ export default {
 
 
 <style lang="scss" scoped>
-
+.profile-picture {
+  height: 10vh;
+  width: 10vh;
+  border-radius: 50%;
+  // box-shadow: 2px 2px 4px black;
+}
 </style>
--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -10,9 +10,38 @@
 
 
 <script>
+import { onMounted, computed,  } from "vue";
+import { useRoute } from "vue-router";
+import { AppState } from "../AppState.js";
+import { profilesService } from "../services/ProfilesService.js";
+import { logger } from "../utils/Logger.js";
+import Pop from "../utils/Pop.js";
+
 export default {
   setup(){
-    return {}
+    const route = useRoute();
+    async function getProfileById(){
+      try {
+        const profileId = route.params.profileId;
+        logger.log('here is the profile id', profileId)
+        await profilesService.getProfileById(profileId)
+        
+      } catch (error) {
+        logger.error(error);
+        Pop.error(error.message);
+      }
+    }
+    onMounted(()=>{
+      getProfileById();
+    });
+
+    onUnmounted(() => {
+      
+    })
+    return {
+      profile: computed(()=> AppState.profile),
+      posts: computed(() => AppState.posts)
+    }
   }
 }
 </script>
