c5b7637170cfcc8d6249959be398502a5f7adb55
Josue Hernandez <josuehdz0@gmail.com>
2023-02-27 02:55:55 -0700

    Starting Adds

---
+++ src/components/AddCard.vue
@@ -0,0 +1,17 @@
+<template>
+ adds will be here
+</template>
+
+
+<script>
+export default {
+  setup(){
+    return {}
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/models/Add.js
@@ -0,0 +1,8 @@
+export class Add{
+  constructor(data){
+    this.title = data.title
+    this.linkURL = data.linkURL
+    this.square = data.square
+    this.banner = data.banner
+  }
+}
